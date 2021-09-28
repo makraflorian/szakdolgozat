6f57fc7f06af65646e52b5392f1170f67e550522
Florian <makraflorian10@gmail.com>
2021-09-28 23:43:08 +0200

    day 1

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# szakdolgozat
+# Szakdolgozat
+Kontrasztjavítás színes képeken
+
+## Chapter 1
+___
+### Day 1
+` Kezdetleges PyQt5 GUI készítése, pygt5-tools install `
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def fileDialog():
+    print('asd')
+
+def main():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(400, 300, 500, 500)
+    win.setWindowTitle("Kontrasztjavítás")
+
+    fileDialogButton = QPushButton(win)
+    fileDialogButton.setText("Choose image")
+    fileDialogButton.move(50, 20)
+    fileDialogButton.clicked.connect(fileDialog)
+
+    win.show()
+    sys.exit(app.exec_())
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi(' masteer')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+main()  # make sure to call the function))
