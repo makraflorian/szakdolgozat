3e60d33dde654193ff6ec697cd9b6292f9243d12
Makra Flórián <h984214@stud.u-szeged.hu>
2022-02-21 21:17:02 +0100

    temp class variable added, so you can try different methods, new button added

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ class Screen2(QDialog):
     def __init__(self):
         super(Screen2, self).__init__()
         self._kep = None
+        self._tempKep = None
         loadUi("screen2.ui", self)
         self.mainWindowBtn.clicked.connect(self.gotoMainWindow)
         self.mainWindowBtn.move(100, 10)
@@ -42,6 +43,13 @@ class Screen2(QDialog):
     def kep(self, ertek):  # set property
         self._kep = ertek
 
+    @property
+    def tempKep(self):  # get property
+        return self._tempKep
+
+    @tempKep.setter
+    def tempKep(self, ertek):  # set property
+        self._tempKep = ertek
 
     def imageThings(self):
         img = getFileName()
@@ -49,36 +57,55 @@ class Screen2(QDialog):
             print("Cancelled ", len(img))
         else:
             self.kep = cv2.imread(img, cv2.IMREAD_COLOR)
+            self.tempKep = self.kep.copy()
             # print(self.kep.shape)
-            pixmap = QPixmap(self.image_cv2qt(self.kep))
+            pixmap = QPixmap(self.image_cv2qt(self.tempKep))
             self.picLabel.setPixmap(pixmap)
             self.picLabel.resize(pixmap.width(), pixmap.height())
 
-            self.magic = QPushButton(self)
-            self.magic.setText("Magic")
-            self.magic.move(300, 15)
-            self.magic.clicked.connect(self.proba)
-            self.magic.setStyleSheet("QPushButton"
-                             "{"
-                             "background-color : lightblue;"
-                             "}"
-                             "QPushButton::pressed"
-                             "{"
-                             "background-color : #4acfff;"
-                             "}"
-                             )
+            self.MSRCP = QPushButton(self)
+            self.MSRCP.setText("MSRCP")
+            self.MSRCP.move(300, 15)
+            self.MSRCP.clicked.connect(self.msrcpCall)
+            self.MSRCP.setStyleSheet("QPushButton"
+                                     "{"
+                                     "background-color : lightblue;"
+                                     "}"
+                                     "QPushButton::pressed"
+                                     "{"
+                                     "background-color : #4acfff;"
+                                     "}"
+                                     )
+            self.MSRCR = QPushButton(self)
+            self.MSRCR.setText("MSRCR")
+            self.MSRCR.move(400, 15)
+            self.MSRCR.clicked.connect(self.msrcrCall)
+            self.MSRCR.setStyleSheet("QPushButton"
+                                     "{"
+                                     "background-color : lightblue;"
+                                     "}"
+                                     "QPushButton::pressed"
+                                     "{"
+                                     "background-color : #4acfff;"
+                                     "}"
+                                     )
 
 
-    def proba(self):
-        self.kep = retinex(self.kep)
+    def msrcpCall(self):
+        self.tempKep = retinexOnIntensity(self.kep)
         # print(self.kep.shape)
         # cv2.imshow('Inverz', self.kep)
         # cv2.waitKey(0)
-        pixmap = QPixmap(self.image_cv2qt(self.kep))
+        pixmap = QPixmap(self.image_cv2qt(self.tempKep))
         self.picLabel.setPixmap(pixmap)
-        cv2.imwrite("output.png", self.kep)
+        cv2.imwrite("output.png", self.tempKep)
 
 
+    def msrcrCall(self):
+        self.tempKep = retinexOnChannels(self.kep)
+        pixmap = QPixmap(self.image_cv2qt(self.tempKep))
+        self.picLabel.setPixmap(pixmap)
+        cv2.imwrite("output.png", self.tempKep)
 
 
     def image_cv2qt(self, img):
